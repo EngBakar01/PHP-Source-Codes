84503e6caa62451b4f85b62ba5bfffbfbdc1d184
unknown <bakaryare2003>
2026-09-19 22:47:45 +0300

    PHP week 1 practices

---
+++ Before_Mid_Term/Week_1/Example.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // Practice #1: Variables
+    // $AGE = 30;
+    // echo "I'm $AGE years old";
+    // print "<br> Welcome to PHP";
+    // define("AGE", 20);
+
+
+    // Practice #2:If Statement
+    // $AGE = 30;
+    // if ($AGE >= 18 ) 
+    //     echo "Your are an adult";
+    // else 
+    //     echo "You are young";
+   
+
+        
+    // Practice #3: if else statement
+    // $Grade = 70;
+    //  if ($Grade >=90)
+    //     echo "A+";
+    // elseif ($Grade >=80)
+    //     echo "B";
+    // elseif ($Grade >=70)
+    //     echo "C+";
+    // else 
+    //     echo "invalid Grade";
+
+
+
+    // Practice #4: Switch Case Statement
+    // $Grade = 50;
+    //   switch ($Grade) {
+    //     case '90':
+    //         echo "A";
+    //         break;
+            
+    //     case '80':
+    //         echo "B";
+    //         break;
+
+    //     case '70':
+    //         echo "C";
+    //         break;
+
+    //     case '50':
+    //         echo "Fail";
+    //         break;
+        
+    //     default:
+    //         echo "no exist";
+    //   }
+
+
+
+    // Practice #5: Logical Operators
+    // $Age = 18;
+    // $Grade = 90;
+    //   if ($Age >=18 && $Grade >= 90)
+    //     echo "Mechanical Engineering";
+    // elseif ($Age >=18 && $Grade >= 90)
+    //     echo "Electrical Engineering";
+    // elseif ($Age >=18 && $Grade >= 90)
+    //     echo "Civil Engineering";
+    // else 
+    //     echo "invalid Grade";
+
+      
+    ?>
+</body>
+</html>
