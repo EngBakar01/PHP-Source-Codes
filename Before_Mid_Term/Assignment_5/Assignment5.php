4e6d6dff725637cb9078ee76c645b78f89826c14
unknown <bakaryare2003>
2026-09-25 08:41:20 +0300

    Updated  Assignments 5

--- Before_Mid_Term/Assignment_5/Assignment5.php
+++ Before_Mid_Term/Assignment_5/Assignment5.php
@@ -7,12 +7,13 @@
 </head>
 <body>
     <?php
-    // write that finds the reverse of the given number from 1 to 5 like 5 to 1 do't use strev use loop
+    // write that finds the reverse of the given number from 1 to 5 like 5 to 1 do't use built in function such as strev function
     $number = 12345;
+    echo "The Given number is: " . $number . "<br>";
     $reverse = 0;
     while ($number > 0) {
-        $remainder = $number % 10;
-        $reverse = ($reverse * 10) + $remainder;
+        $digit = $number % 10;
+        $reverse = ($reverse * 10) + $digit;
         $number = (int)($number / 10);
     }
     echo "Reverse of the given number is: " . $reverse;
